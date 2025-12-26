7678a24942f2fdaf8c9fa7b88cfe8c5638e36d7c
Leonid Kostrykin <void@evoid.de>
2025-12-26 13:05:27 +0100

    Fix bug (#12)
    Fix Bug: CI did not fail when tests failed.

---
+++ example/tests/integration/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class EmptyTest(unittest.TestCase):
+    """
+    This test is to make the CI pass when no integration tests are implemented (yet).
+    """
+
+    def test(self):
+        pass
